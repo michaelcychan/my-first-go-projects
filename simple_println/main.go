7e7b3d149ba6ec962b881e73e112f38440d18a1c
Michael Chan <michaelcychan@gmail.com>
2022-09-07 17:11:41 +0100

    first go program

---
+++ simple_println/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello World! This is my first local Go Program!")
+	fmt.Println("The time now is")
+	fmt.Println(time.Now())
+	fmt.Println("      `.-::::::-.`")
+	fmt.Println("  .:-::::::::::::::-:.")
+	fmt.Println("  `_:::    ::    :::_`")
+	fmt.Println("   .:( ^   :: ^   ):.")
+	fmt.Println("   `:::   (..)   :::.")
+	fmt.Println("   `:::::::UU:::::::`")
+	fmt.Println("   .::::::::::::::::.")
+	fmt.Println("   O::::::::::::::::O")
+	fmt.Println("   -::::::::::::::::-")
+	fmt.Println("   `::::::::::::::::`")
+	fmt.Println("    .::::::::::::::.")
+	fmt.Println("      oO:::::::Oo")
+}
