1ba35c875530303e3bdbca3733c498d59bc314af
Michael Chan <michaelcychan@gmail.com>
2022-09-08 15:53:23 +0100

    update fizzbuzz

--- fizzbuzz/first_fizzbuzz_go/main.go
+++ fizzbuzz/first_fizzbuzz_go/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func fizzbuzz(number int) string {
-	if number%15 == 0 {
+	if divisibleBy(number, 15) {
 		return "FizzBuzz"
-	} else if number%5 == 0 {
+	} else if divisibleBy(number, 5) {
 		return "Buzz"
-	} else if number%3 == 0 {
+	} else if divisibleBy(number, 3) {
 		return "Fizz"
 	} else {
 		return fmt.Sprint(number)
+	}
+}
+
+func divisibleBy(dividend, divisor int) bool {
+	if dividend%divisor == 0 {
+		return true
+	} else {
+		return false
 	}
 }
 
