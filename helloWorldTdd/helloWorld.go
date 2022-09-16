2ca0fd2189662248e079840a4b9ad36e964a3473
Michael Chan <michaelcychan@gmail.com>
2022-09-16 18:17:32 +0100

    refactor tdd world

--- helloWorldTdd/helloWorld.go
+++ helloWorldTdd/helloWorld.go
@@ -1,7 +1,10 @@
 package main
 
-func HelloWorld(name string) string {
-	return "Hello " + correctInput(name) + "!"
+import "fmt"
+
+func HelloWorld(name, lang string) string {
+	wholeSentence := generateReply(name, lang)
+	return wholeSentence
 }
 
 func correctInput(name string) string {
@@ -10,6 +13,19 @@ func correctInput(name string) string {
 	} else {
 		return name
 	}
+}
+
+func generateReply(name, lang string) string {
+	var sentence string
+	switch lang {
+	case "jp":
+		sentence = fmt.Sprintf("こにちは、%v!", correctInput(name))
+	case "zh":
+		sentence = fmt.Sprintf("你好，%v!", correctInput(name))
+	default:
+		sentence = fmt.Sprintf("Hello %v!", correctInput(name))
+	}
+	return sentence
 }
 
 func main() {
--- helloWorldTdd/helloWorld_test.go
+++ helloWorldTdd/helloWorld_test.go
@@ -4,19 +4,31 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	t.Run("saying hello world for empty input", func(t *testing.T) {
-		got := HelloWorld("")
+		got := HelloWorld("", "en")
 		want := "Hello World!"
 		assertionMessage(t, got, want)
 	})
 	t.Run("say hello to Mike if input is Mike", func(t *testing.T) {
-		got := HelloWorld("Mike")
+		got := HelloWorld("Mike", "en")
 		want := "Hello Mike!"
 
 		assertionMessage(t, got, want)
 	})
 	t.Run("Say hello to Tom if input is Tom", func(t *testing.T) {
-		got := HelloWorld("Tom")
+		got := HelloWorld("Tom", "en")
 		want := "Hello Tom!"
+
+		assertionMessage(t, got, want)
+	})
+	t.Run("Say こにちは to ゆき if lang is japanese", func(t *testing.T) {
+		got := HelloWorld("ゆき", "jp")
+		want := "こにちは、ゆき!"
+
+		assertionMessage(t, got, want)
+	})
+	t.Run("Say 你好 to 偉仔 if lang is zh", func(t *testing.T) {
+		got := HelloWorld("偉仔", "zh")
+		want := "你好，偉仔!"
 
 		assertionMessage(t, got, want)
 	})
@@ -25,6 +37,6 @@ func TestHelloWorld(t *testing.T) {
 func assertionMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q but got %q", got, want)
+		t.Errorf("got %q but want %q", got, want)
 	}
 }
