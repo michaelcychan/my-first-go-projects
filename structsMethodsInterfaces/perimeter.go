c34fb1a8204ff744724b53b2ca0141d711c1b3fc
Michael Chan <michaelcychan@gmail.com>
2022-09-19 22:36:56 +0100

    change area functions to methods

--- structsMethodsInterfaces/perimeter.go
+++ structsMethodsInterfaces/perimeter.go
@@ -1,14 +1,24 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(height, width float64) float64 {
-	return (height + width) * 2
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
 
-func Area(height, width float64) float64 {
-	return height * width
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Height + rectangle.Width) * 2
 }
