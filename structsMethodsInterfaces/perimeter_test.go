c5a51e99ef0c89b6ba16a114324b0b1ddee90eb0
Michael Chan <michaelcychan@gmail.com>
2022-09-19 22:56:19 +0100

    added interface

--- structsMethodsInterfaces/perimeter_test.go
+++ structsMethodsInterfaces/perimeter_test.go
@@ -13,23 +13,35 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		expected := 72.0
-
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != expected {
 			t.Errorf("got %.2f, but expected %.2f", got, expected)
 		}
+	}
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
 	})
 	t.Run("circle area", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		expected := 314.1592653589793
-
-		if got != expected {
-			t.Errorf("got %g, but expected %g", got, expected)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
+}
 
+func TestAreaTable(t *testing.T) {
+	areaTests := []struct {
+		shape    Shape
+		expected float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+	for _, testElement := range areaTests {
+		got := testElement.shape.Area()
+		if got != testElement.expected {
+			t.Errorf("got %g, but wanted %g", got, testElement.expected)
+		}
+	}
 }
