1e4c99fd1148758805e64855e8fdd6be1c1de9ee
Michael Chan <michaelcychan@gmail.com>
2022-11-22 16:44:16 +0000

    add interface and struct in creating types

--- struct-methods-interfaces/shapes.go
+++ struct-methods-interfaces/shapes.go
@@ -1,12 +1,26 @@
 package shape
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
 type Circle struct {
 	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- struct-methods-interfaces/shapes_test.go
+++ struct-methods-interfaces/shapes_test.go
@@ -15,22 +15,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("returns 100.0 as area of a rectangle with all sides as 10.0", func(t *testing.T) {
-		rect := Rectangle{10.0, 10.0}
-		got := Area(rect)
-		expected := 100.0
+
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
 
 		if got != expected {
 			t.Errorf("got %.2f, but expected %.2f.", got, expected)
 		}
+	}
+
+	t.Run("returns 100.0 as area of a rectangle with all sides as 10.0", func(t *testing.T) {
+		rect := Rectangle{10.0, 10.0}
+		checkArea(t, rect, 100.0)
 	})
 	t.Run("return 314.159... for an area of circle with radius as 10.0", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := Area(circle)
-		expected := 314.1592653589793
-
-		if got != expected {
-			t.Errorf("got %g, but expected %g.", got, expected)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
