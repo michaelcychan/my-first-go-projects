f4a91b721eda5e296aff35df17804927cda48e66
Michael Chan <michaelcychan@gmail.com>
2022-11-23 14:49:12 +0000

    learn create custom type from original types, customise error message and how to assert error, create global variable for err message

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,16 +1,36 @@
 package wallet
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
 
 type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
 }
 
-func (wal *Wallet) Deposit(money Bitcoin) {
-	fmt.Printf("address of balance in function is %v \n", &wal.balance)
-	(*wal).balance += money
+func (wal *Wallet) Deposit(amount Bitcoin) {
+	(*wal).balance += amount
+}
+
+var ErrInsufficientFund = errors.New("cannot withdraw, insufficient fund")
+
+func (wal *Wallet) Withdraw(amount Bitcoin) error {
+	if wal.balance >= amount {
+		wal.balance -= amount
+		return nil
+	}
+	return ErrInsufficientFund
 }
 
 func (wal *Wallet) Balance() Bitcoin {
--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -1,22 +1,52 @@
 package wallet
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	gotBalance := wallet.Balance()
+		expectedBalance := Bitcoin(10)
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		assertBalance(t, wallet, expectedBalance)
 
-	expectedBalance := Bitcoin(10)
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
 
-	if gotBalance != expectedBalance {
-		t.Errorf("expected %d, but got %d", expectedBalance, gotBalance)
+		expectedBalance := Bitcoin(10)
+
+		assertBalance(t, wallet, expectedBalance)
+	})
+	t.Run("Withdraw when insufficient fund", func(t *testing.T) {
+		initialBalance := Bitcoin(20)
+		wallet := Wallet{balance: initialBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFund)
+		assertBalance(t, wallet, initialBalance)
+
+	})
+}
+
+func assertBalance(t testing.TB, wall Wallet, expectedBalance Bitcoin) {
+	t.Helper()
+	if wall.Balance() != expectedBalance {
+		t.Errorf("expected %s, but got %s", expectedBalance, wall.Balance())
+	}
+}
+
+func assertError(t testing.TB, got error, expected error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("Expected an error, but did not get one") // t.Fatal will stop the whole test
+	}
+	if got != expected {
+		t.Errorf("Expected %q, but got %q", expected, got)
 	}
 }
