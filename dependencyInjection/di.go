1d92781e5da8803182c2d8db1cccb76d458e4476
Michael Chan <michaelcychan@gmail.com>
2022-11-25 17:10:23 +0000

    add internet usage instead of stdout

--- dependencyInjection/di.go
+++ dependencyInjection/di.go
@@ -1,15 +1,31 @@
-package di
+package main
 
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+
+	// "os" // used only for original main
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+// Original main without Internet function
+//
+// func main() {
+// 	Greet(os.Stdout, "Elodie")
+// }
+
+// Greet function can be used with os.Stdout and http.ResponseWriter,
+// because both of them are using io.Writer interface.
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
