117cc522ffa2a9260a0335b3fbb42041ec936b9c
Michael Chan <michaelcychan@gmail.com>
2022-12-21 12:19:51 +0000

    created struct test cases list for reflection chapter

---
+++ reflection/reflection_test.go
@@ -0,0 +1,44 @@
+// From Twitter
+// golang challenge:
+// write a function walk(x interface{}, fn func(string))
+// which takes a struct x and
+// calls fn for all strings fields found inside.
+// difficulty level: recursively.
+
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("expected input processed: %v, but got %v", test.ExpectedCalls, got)
+			}
+
+		})
+	}
+}
