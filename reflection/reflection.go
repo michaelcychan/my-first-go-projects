2650fb922196a979d770b9c7c2c1c4c62a4d2551
Michael Chan <michaelcychan@gmail.com>
2023-01-05 18:48:12 +0000

    refactor the reflection chapter for consistent codes among array, slices, struct and map

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -18,57 +18,56 @@ func getValue(x interface{}) reflect.Value {
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	var numOfField int = 0
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
-	// this is to declare a variable,
-	// this variable will be a function taking an int and return a reflect.Value
-	var getField func(int) reflect.Value
+	// obsolete
+	// // this is to declare a variable,
+	// // this variable will be a function taking an int and return a reflect.Value
+	// var getField func(int) reflect.Value
+	// var numOfField int = 0
 
 	switch val.Kind() {
 	// if x is already string, just run the function
 	case reflect.String:
 		fn(val.String())
 
-	// // .NumField() and .Field() will cause panic if not used on struct
-	// // that is why in our test case, the simplest example is still a struct
-	// case reflect.Struct:
-	// 	for i := 0; i < val.NumField(); i++ {
-	// 		field := val.Field(i)
-	// 		walk(field.Interface(), fn)
-	// 	}
-
-	// // Slice does not take .NumField() or .Field(), so we need another case
-	// // because val is a reflect object, we need to use .Len() and .Index(i)
-	// case reflect.Slice:
-	// 	for i := 0; i < val.Len(); i++ {
-	// 		field := val.Index(i)
-	// 		walk(field.Interface(), fn)
-	// 	}
-	// 	return
-
 	// .NumField() and .Field() will cause panic if not used on struct
 	// that is why in our test case, the simplest example is still a struct
-
-	// instead of the old method,
-	// the refactored code keeps only getting values and fields num in switch
 	case reflect.Struct:
-		numOfField = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 
 	// Slice does not take .NumField() or .Field(), so we need another case
 	// because val is a reflect object, we need to use .Len() and .Index(i)
 	case reflect.Slice, reflect.Array:
-		numOfField = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+		return
+
+	// // the below was intermin refactor,
+	// // the refactored code keeps only getting values and fields num in switch
+	// // but this way does not work for map, so reverted back
+	// case reflect.Struct:
+	// 	numOfField = val.NumField()
+	// 	getField = val.Field
+
+	// // Slice does not take .NumField() or .Field(), so we need another case
+	// // because val is a reflect object, we need to use .Len() and .Index(i)
+	// case reflect.Slice, reflect.Array:
+	// 	numOfField = val.Len()
+	// 	getField = val.Index
 	case reflect.Map:
 
 		// the following code will print keys rather than values
 		// for _, key := range val.MapKeys() {
 		// 	fn(key.String())
 		// }
-
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
 	}
 
@@ -77,8 +76,9 @@ func walk(x interface{}, fn func(input string)) {
 	// for slice and struct, they get the numOfField inside switch,
 	// and they their respective .Field() and .Index() is copied to getField
 
-	for i := 0; i < numOfField; i++ {
-		walk(getField(i).Interface(), fn)
-	}
+	// obsolete
+	// for i := 0; i < numOfField; i++ {
+	// 	walk(getField(i).Interface(), fn)
+	// }
 
 }
