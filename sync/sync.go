0d1137f36174bf6e0d0e9a8c9dde6c5bc6240d36
Michael Chan <michaelcychan@gmail.com>
2023-01-09 13:13:34 +0000

    sync chapter - add sync.Mutex

--- sync/sync.go
+++ sync/sync.go
@@ -1,11 +1,20 @@
 package sync
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	// lock Counter so other goroutine can't run
+	// Unlock Counter after current goroutine is done
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.value += 1
+
 }
 
 func (c *Counter) Value() int {
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times, returns 3", func(t *testing.T) {
@@ -9,8 +12,33 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("expected %d, but got %d", 3, counter.Value())
-		}
+		assertCounterValue(t, 3, counter.Value())
 	})
+	t.Run("runs safely concurrently", func(t *testing.T) {
+		expectedCounter := 2000
+		counter := Counter{}
+
+		// sync.WaitGroup wold wait a number of goroutine to finish
+		// .Add tells WaitTroup how many goroutines there are
+		var wg sync.WaitGroup
+		wg.Add(expectedCounter)
+
+		for i := 0; i < expectedCounter; i++ {
+			go func() {
+				counter.Inc()
+				// .Done() ensures WaitGroup knows this is one goroutine to perform
+				wg.Done()
+			}()
+		}
+		// .Wait() makes sure the goroutines are finished before moving on
+		wg.Wait()
+
+		assertCounterValue(t, expectedCounter, counter.Value())
+	})
+}
+
+func assertCounterValue(t testing.TB, expected, got int) {
+	if expected != got {
+		t.Errorf("expected %d, but got %d", expected, got)
+	}
 }
