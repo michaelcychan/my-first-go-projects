5aa0900953187f49df429d6d41f42d0ec9c333c6
Michael Chan <michaelcychan@gmail.com>
2023-01-09 21:37:49 +0000

    begin context chapter

---
+++ context/server_test.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func TestServer(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		data := "hello, world"
+		svr := Server(&SpyStore{response: data})
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf("expected %s, but got %s", data, response.Body)
+		}
+	})
+	t.Run("triggers store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("sotre was not told to cancel")
+		}
+	})
+}
